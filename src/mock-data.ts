be77ea89ebad91bd76788abea9d612273f8b01fc
NoistNT <arielgnr23@gmail.com>
2024-07-05 23:30:05 -0300

    Add mock data

---
+++ src/mock-data.ts
@@ -0,0 +1,41 @@
+export const products = [
+  {
+    name: 'Samsung Galaxy S23 Ultra',
+    brand: 'Samsung',
+    price: 1249.99,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/dsg5ofk4e/image/upload/v1710445336/64cb1ba09a7d078af5504e51a14e1c5a.webp',
+    description:
+      "The Samsung Galaxy S23 Ultra is a powerful Android smartphone with a large 6.8-inch display, a quad-lens camera system, and a long-lasting battery. It's available in various storage capacities.",
+    category: 'Phone',
+    stock: 50,
+    isActive: true,
+  },
+  {
+    name: 'Dell XPS 13',
+    brand: 'Dell',
+    price: 999.99,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/dsg5ofk4e/image/upload/v1710445336/64cb1ba09a7d078af5504e51a14e1c5a.webp',
+    description:
+      'A thin and light laptop with a beautiful display, long battery life, and powerful performance.',
+    category: 'Laptop',
+    stock: 20,
+    isActive: true,
+  },
+  {
+    name: 'Sony WH-1000XM5',
+    brand: 'Sony',
+    price: 499.99,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/dsg5ofk4e/image/upload/v1710445336/64cb1ba09a7d078af5504e51a14e1c5a.webp',
+    description:
+      'Industry-leading noise cancellation, exceptional sound quality, and comfortable design.',
+    category: 'Headphones',
+    stock: 35,
+    isActive: true,
+  },
+];
